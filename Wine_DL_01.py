178111da2bbf8f4a444553a5c227a1b35e8af2bb
piyo-toyou <61530828+piyo-toyou@users.noreply.github.com>
2021-09-09 12:41:38 +0900

    stage2

--- Wine_DL_01.py
+++ Wine_DL_01.py
@@ -1,8 +1,55 @@
 import Wine_Datasete_02 as wd2
 import torch
+from torch import nn
 from torch.utils.data import DataLoader
 
 # Dataset を作成する。
 dataset = wd2.Wine("https://git.io/JfodD")
 # DataLoader を作成する。
 dataloader = DataLoader(dataset, batch_size=64)
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = NeuralNetwork()
+
+learning_rate = 1e-3
+batch_size = 64
+epochs = 10
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+  size = len(dataloader.dataset)
+  for batch, (X, y) in enumerate(dataloader):
+    pred = model(X)
+    loss = loss_fn(pred, y)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if batch % 100 == 0:
+      loss, current = loss.item(), batch * len(X)
+      print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(dataloader, model, loss_fn, optimizer)
+print("Done!")
