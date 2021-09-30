fc08313a58a98bf746165f1358646165a15b6261
piyo-toyou <61530828+piyo-toyou@users.noreply.github.com>
2021-09-30 09:39:33 +0900

    stage13

--- WL_CSV_CAL_01.py
+++ WL_CSV_CAL_01.py
@@ -8,13 +8,17 @@ def excel_get(path, header=0, sheet_name="Sheet1"):
     rain = df["Rain_mm"].values
     return gwl, rwl, rain
 
-def rwl_cal(rwl, n):
-    rwl_dict = {}
+def time_slide(arr, n):
+    arr_dict = {}
+    arr_dict[0] = arr
     for i in range(1,n+1):
-         rwl_dict[i] = rwl
+         arr_dict[i] = np.append(np.nan, arr_dict[i-1])
+         arr_dict[i] = np.delete(arr_dict[i], -1)
+    return arr_dict
 
-def rain_cal():
-    pass
+def diff_cal(my_dict):
+    diff_arr = my_dict[0] - my_dict[1]
+    return diff_arr
 
 my_xlsx = "Z://(新)技術課/CG20-0155-41 山財ダム/観測データ/時系列データとりまとめ.xlsx"
 gwl, rwl, rain = excel_get(my_xlsx, header=0, sheet_name="EL")
